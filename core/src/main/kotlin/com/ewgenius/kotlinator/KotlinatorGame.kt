ad4831a4f0c5cd4192ca6e8adba1794f61314e51
ewgenius <ewgeniux@gmail.com>
2015-11-26 17:55:51 +0300

    upd

--- core/src/main/kotlin/com/ewgenius/kotlinator/KotlinatorGame.kt
+++ core/src/main/kotlin/com/ewgenius/kotlinator/KotlinatorGame.kt
@@ -3,7 +3,6 @@ package com.ewgenius.kotlinator
 import com.badlogic.gdx.Game
 import com.ewgenius.kotlinator.screens.MenuScreen
 
-
 class KotlinatorGame : Game() {
     override fun create() {
         val menuScreen: MenuScreen = MenuScreen(this)
