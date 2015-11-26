d44f32edbf93d2b8c68e81b65c82c70887c65d0e
ewgenius <ewgeniux@gmail.com>
2015-11-26 17:40:14 +0300

    upd with screens

--- core/src/main/kotlin/com/ewgenius/kotlinator/KotlinatorGame.kt
+++ core/src/main/kotlin/com/ewgenius/kotlinator/KotlinatorGame.kt
@@ -1,25 +1,12 @@
 package com.ewgenius.kotlinator
 
-import com.badlogic.gdx.ApplicationAdapter
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.Game
+import com.ewgenius.kotlinator.screens.MenuScreen
 
-class KotlinatorGame : ApplicationAdapter() {
-    internal var batch: SpriteBatch? = null
-    internal var img: Texture? = null
 
+class KotlinatorGame : Game() {
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
-    }
-
-    override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch?.begin()
-        batch?.draw(img, 0f, 0f)
-        batch?.end()
+        val menuScreen: MenuScreen = MenuScreen(this)
+        setScreen(menuScreen)
     }
 }
---
+++ core/src/main/kotlin/com/ewgenius/kotlinator/screens/MenuScreen.kt
@@ -0,0 +1,26 @@
+package com.ewgenius.kotlinator.screens
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.ScreenAdapter
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.ewgenius.kotlinator.KotlinatorGame
+
+class MenuScreen(game: KotlinatorGame): ScreenAdapter() {
+    internal var batch: SpriteBatch? = null
+    internal var img: Texture? = null
+
+    override fun show() {
+        batch = SpriteBatch()
+        img = Texture("badlogic.jpg")
+    }
+
+    override fun render(deltatime: Float) {
+        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch?.begin()
+        batch?.draw(img, 0f, 0f)
+        batch?.end()
+    }
+}
