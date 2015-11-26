af426664e8b2e16a71da60b0208722b8d3acd1b9
ewgenius <ewgeniux@gmail.com>
2015-11-26 16:24:12 +0300

    init

---
+++ android/src/com/ewgenius/kotlinator/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ewgenius.kotlinator.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ewgenius.kotlinator.KotlinatorGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KotlinatorGame(), config);
+	}
+}
---
+++ ios/src/com/ewgenius/kotlinator/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.ewgenius.kotlinator;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.ewgenius.kotlinator.KotlinatorGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new KotlinatorGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
