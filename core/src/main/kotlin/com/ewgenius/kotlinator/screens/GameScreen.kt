6c08ac0dec12daa68d5ada96d6cc6f389e12123d
ewgenius <ewgeniux@gmail.com>
2015-11-27 07:34:31 +0300

    upd

--- core/src/main/kotlin/com/ewgenius/kotlinator/screens/GameScreen.kt
+++ core/src/main/kotlin/com/ewgenius/kotlinator/screens/GameScreen.kt
@@ -5,24 +5,22 @@ import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.ewgenius.kotlinator.KotlinatorGame
 
-class MenuScreen(game: KotlinatorGame): ScreenAdapter() {
+class GameScreen(game: KotlinatorGame): ScreenAdapter() {
     internal var batch: SpriteBatch? = null
-    internal var skin:Skin? = null
-    internal var buttonStart: TextButton? = null
-    internal var buttonAbout: TextButton? = null
-    internal var buttonExit: TextButton? = null
+    internal var img: Texture? = null
 
     override fun show() {
         batch = SpriteBatch()
-
-
+        img = Texture("badlogic.jpg")
     }
 
     override fun render(deltatime: Float) {
-
+        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch?.begin()
+        batch?.draw(img, 0f, 0f)
+        batch?.end()
     }
 }
